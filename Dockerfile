76703ff9a463f07791a0b65ed23d54b5027f2d1a
Nathan Robertson <nathanr@lesmills.com.au>
2022-01-18 12:28:02 +1100

    Add a fixed datatypes.py to prove the fix works

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.8-buster
+ENV pyver="3.8"
 
 COPY test_schema.xsd /test/
 
@@ -6,6 +7,9 @@ RUN \
     pip install PyXB-X==1.2.6 python-dateutil==2.8.2 && \
     cd /test && \
     pyxbgen -u ./test_schema.xsd -m protocol --binding-root=generated
+
+# Comment out to get the original crash. This copies in the patched file.
+COPY fixed-datatypes.py "/usr/local/lib/python${pyver}/site-packages/pyxb/binding/datatypes.py"
 
 COPY test.py /test/
 WORKDIR /test
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from datetime import datetime, date, timedelta
+from wsgiref.simple_server import sys_version
 from dateutil.relativedelta import relativedelta
+import sys
 
 from generated import protocol
+
+print("Python version is: {}.{}".format(sys.version_info[0], sys.version_info[1]))
 
 # This does not fail. It doesn't hit PyXB code. However, it is equivalent.
 equivalent_base_python_test = date.today() + relativedelta(months = + 2) + relativedelta(days = -3)
@@ -9,10 +13,10 @@ print("Base python reports today + 2 months - 3 days is: {}".format(equivalent_b
 
 period = protocol.someType()
 period.lastUpdated = datetime.now()
-print("Last updated is: {}".format(period.lastUpdated))
+print("Last updated is: {} (type {}, module {})".format(period.lastUpdated, type(period.lastUpdated).__name__, type(period.lastUpdated).__module__))
 period.zeroDate = date.today() + relativedelta(months = + 2) + relativedelta(days = -3)
-print("zeroDate is: {}".format(period.zeroDate))
+print("zeroDate is: {} (type {}, module {})".format(period.zeroDate, type(period.zeroDate).__name__, type(period.zeroDate).__module__))
 period.oneDate = date.today() + relativedelta(months = + 2)
-print("oneDate is: {}".format(period.oneDate))
+print("oneDate is: {} (type {}, module {})".format(period.oneDate, type(period.oneDate).__name__, type(period.oneDate).__module__))
 period.twoDate = period.oneDate + relativedelta(days = -3)
-print("twoDate is: {}".format(period.twoDate))
+print("twoDate is: {} (type {}, module {})".format(period.twoDate, type(period.twoDate).__name__, type(period.twoDate).__module__))
